0861c1dd4317b785dc31d668798f88d95a500d35
HosanaSF <hosanahsf@gmail.com>
2021-09-06 21:50:09 -0300

    subindo arquivo com modelagem da tabela compra

--- src/models/compra.js
+++ src/models/compra.js
@@ -2,17 +2,14 @@ const compra = (sequelize, DataTypes) => {
     const Compra = sequelize.define('Compra', {
         idCompra: {
             type: DataTypes.INTEGER,
-            autoIncrement: true,
             unique: true,
             allowNull: false,
             primaryKey: true
         },
         idCliente: {
             type: DataTypes.INTEGER,
-            autoIncrement: true,
             unique: true,
             type: DataTypes.INTEGER,
-            autoIncrement: true,
             unique: true,
             allowNull: false,
             foreignKey: true
