78c5a7063da2890bc81294b620ad79d0a1254569
Leticianunes2 <leticianunes29021992@gmail.com>
2021-09-12 16:12:52 -0300

    correcao service compras

--- src/services/compras.js
+++ src/services/compras.js
@@ -1,46 +1,22 @@
-const express = require("express");
-const router = express.Router();
-const { compra } = require("../models");
-const CompraService = require("../service/compra");
-const auth = require("./functions");
-
-const compraService = new CompraService(compra);
-
-
-
-router.put("/compra", auth, async (req, res) => {
-    //const compra = await compraService.put();
-    //res.status(200).json(compra);
-});
-
-router.put("/", auth, async (req, res) => {
-
-});
-
-router.put("/", auth, async (req,res) => {
-
-});
-
-router.put("/", auth, async (req, res) => {
-
-});
-
-router.get("/", auth, async (req, res) => {
-    const compra = await compraService.get();
-    res.status(200).json(compra);
-});
-
-
-router.get("/", auth, async (req, res) => {
-    const { idCompra } = req.params 
-
-    try {
-        const compra = await compraService.findById(idCompra).populate({id: idProduto}).populate({id: idcliente})
-        return res.status(200).json(compra)
-    
-    }catch(error){
-        return res.status(400).json(error)
+class CompraService {
+    constructor (CompraModel) {
+      this.compra = CompraModel
     }
-})
-
-module.exports = router;
+  
+    async get () {
+      const compras = await this.compra.findAll()
+      return compras
+    }
+  
+    async cadastrar (compraDTO) {
+          
+      try {
+        await this.compra.create(compraDTO)
+      } catch (erro) {
+        console.erro(erro.message)
+        throw erro
+      }
+    }
+  }
+        
+  module.exports = CompraService
