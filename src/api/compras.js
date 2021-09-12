8efe45eb7ffa1cdc6841676349dc77058a4d7ac8
Leticianunes2 <leticianunes29021992@gmail.com>
2021-09-12 16:09:12 -0300

    correcao get e post compras

--- src/api/compras.js
+++ src/api/compras.js
@@ -1,58 +1,31 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router ();
-const { compras } = require(`../models`);
-const ComprasService = require(`../services/compras`);
-const auth = require ("./functions");
+const { compra } = require('../models');
+const CompraService = require('../services/compras');
+const autenticacao = require ('./functions');
 
-const comprasService = new ComprasService(compras);
+const compraService = new CompraService(compra);
 
-router.post ("/compra" ,auth, async (req, res) => {
-    const {idCliente, idLoja, data, itens, valorTotal, pagamento, status} = req.body;
+router.get('/', autenticacao, async (req, res) => {
+    const compras = await compraService.get(); 
+    // colocar o filtro de id do cliente, fazer o join dos produtos
+    res.status(200).json(compras);
+});
+
+router.post (
+    '/compra', 
+    autenticacao, 
+    //fazer validação    
+    async (req, res) => {
+    const { idLoja, idCliente, data, valorTotal, pagamento, status } = req.body;
     try{
-        await compra.create ({idCliente, idLoja, data, itens, valorTotal, pagamento, status});
-        res.status(201).send("compra criada com sucesso");
-
+        await compra.create({idLoja, idCliente, data, valorTotal, pagamento, status});
+        res.status(201).send('compra criada com sucesso');
     }catch(erro){
-        res.status(400).send("erro ao cadastrar compra");
-        
+        res.status(400).send('erro ao cadastrar compra');
     }
 });
 
-
-
-router.put("/",auth,async (req, res) => {
-   // const compras = await comprasService.put();
-    //res.status(200).json(compras);
-});
-
-router.put ("/", auth, async (req, res) => {
-
-});
-
-router.put("/",auth, async (req, res) => {
-
-});
-
-router.put("/", auth, async (req, res) => {
-
-})
-
-router.get("/", auth, async (req, res) => {
-    const compra = await comprasService.get();
-    res.status(200).json(compra);
-});
-
-
-//router.get("/",auth, async (req, res) => {
-    //const { idCompra }= req.params
-
-    //try {
-        //const compra = await comprasService.findById(idCompra).populate({ id: idCompra})
-            //return res.status(200).json(compra)
-
-             //}catch(error){
-                // return res.status(400).json(error)
-             //}
-   // })
-
 module.exports = router;
+
+
