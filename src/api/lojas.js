820c6ca81c7544822f591f1ef3fd386e977b9539
Celiane Brasil <celianebrasil94@gmail.com>
2021-09-13 16:42:44 -0300

    commit final

--- src/api/lojas.js
+++ src/api/lojas.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const { loja } = require('../models');
 const LojaService = require('../services/lojas');
@@ -8,6 +8,25 @@ const autenticacao = require('./functions');
 const lojaService = new LojaService(loja);
 
 router.get('/', autenticacao, async (req, res) => {
+  /*
+    #swagger.tags = ['Lojas']
+    #swagger.description = 'Endpoint parra obter uma lista de lojas' 
+
+    #swagger.security = [{
+      "apiKeyAuth": []
+    }]
+    
+    #swagger.responses[200] = {
+      schema: { $ref: "#/definitions/Loja"},
+      description: 'Loja encontrada'
+    }
+    #swagger.responses[404] = {
+      description: 'Loja não encontrada'
+    }
+    #swagger.responses[400] = {
+      description: 'Desculpe, tivemos um problema com a requisição'
+    }
+  */
   const lojas = await lojaService.get();
   res.status(200).json(lojas);
 })
--- src/models/cliente.js
+++ src/models/cliente.js
@@ -1,18 +1,19 @@
 const cliente = (sequelize, DataTypes) => {
     const Cliente = sequelize.define('Cliente', {
-        nomeCompleto: {
-            type: DataTypes.STRING(100)
-        },
-        cpf: {
-            type: DataTypes.STRING(11)
-        }, 
-        endereco: {
-            type: DataTypes.STRING(150)
-        }
-    }, {
-        tablename: 'cliente'
-    })
-    return cliente
+        nome: DataTypes.STRING(100),
+        cpf: DataTypes.STRING(11),
+        endereco: DataTypes.STRING(150),
+        email: DataTypes.STRING(150),
+        usuario_id: DataTypes.INTEGER
+    }, { timestamps: false, tableName: 'cliente' })
+    
+    Cliente.associate = (models) => {
+        Cliente.belongsTo(models.Usuario, { 
+            constraint: true,
+            foreignKey: 'usuario_id'
+        });   
+      }; 
+    return Cliente
 }
 
-module.exports = cliente
+module.exports = cliente;
